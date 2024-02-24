9422194383047742551270b5d8fc999dea9652c6
YuriyBoyko <3dvision13@gmail.com>
2024-02-24 17:44:07 +0200

    rename HW_2

---
+++ Boyko_HW_Git/HW_2_Terraform/networking/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr" {
+  description = ""
+}
+
+variable "vpc_cidr_public" {
+  description = ""
+}
+
+variable "aws_region" {
+  description = ""
+}
+
+variable "vpc_cidr_private" {
+  description = ""
+}
