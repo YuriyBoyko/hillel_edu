cc1e3f18364014a243da49fadcab3720c513bf12
YuriyBoyko <3dvision13@gmail.com>
2024-02-26 22:05:09 +0200

    correct HW_Docker

--- Boyko_HW_Git/HW_Docker_Boyko/docker_ngx_start.sh
+++ Boyko_HW_Git/HW_Docker_Boyko/docker_ngx_start.sh
@@ -1,13 +1,8 @@
-#docker pull nginx
-#sleep 2s
-mkdir -p ~/docker/nginx
-mkdir -p ~/docker/nginx/html
-echo "Hello. This is my Nginx started page" > ~/docker/nginx/html/index.html
-echo "FROM nginx" > ~/docker/nginx/Dockerfile
-echo "COPY html /usr/share/nginx/html" >> ~/docker/nginx/Dockerfile
-docker build -t my_image_nginx $HOME/docker/nginx
+#!/bin/bash
+
+docker build -t edu_image_nginx .
 sleep 3s
-docker run -d --name nginx -p 80:80 my_image_nginx
+docker run -d --name nginx -p 80:80 edu_image_nginx
 sleep 3s
 echo -e "\n"
 curl 127.0.0.1
